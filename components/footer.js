73ae2c7cd40d441da232da9868ea321c18fd49e7
Jeff_Gore <JefGore@gmail.com>
2022-01-14 13:32:17 -0500

    adjust footer layout

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,24 @@
 const Footer = () => {
   return (
     <footer>
-      
+      <div id="contact-info">
+            <h3 className="contact">CONTACT:</h3>
+              <a
+                href="https://www.linkedin.com/in/jeffgore77/"
+                target="_blank">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/JGore99" target="_blank">
+                <i class="fab fa-github"></i>
+              </a>
+              <div class="email-container"
+                style={{
+                  fontSize: "0.82rem",
+                }}>
+                <i class="far fa-envelope">: </i>
+                <h5 class="email">JefGore@gmail.com</h5>
+              </div>
+          </div>
       <p class="footer-text">© 2022 Jeffrey Gore. All rights reserved.</p>
     </footer>
   );
--- pages/index.js
+++ pages/index.js
@@ -307,24 +307,6 @@ const Index = ({ allMD }) => {
             </div>
           </section>
           </section>
-          <section id="contact-info">
-            <h3 className="contact">CONTACT:</h3>
-              <a
-                href="https://www.linkedin.com/in/jeffgore77/"
-                target="_blank">
-                <i class="fab fa-linkedin"></i>
-              </a>
-              <a href="https://github.com/JGore99" target="_blank">
-                <i class="fab fa-github"></i>
-              </a>
-              <div class="email-container"
-                style={{
-                  fontSize: "0.82rem",
-                }}>
-                <i class="far fa-envelope">: </i>
-                <h5 class="email">JefGore@gmail.com</h5>
-              </div>
-          </section>
         </main>
       </div>
     </Layout>
